7aedd23a5d62dc64c34db704aca2da1a0b84ee54
uezcr <75730965+uezcr@users.noreply.github.com>
2023-02-01 20:53:36 +0800

    ModularGameplayActorsPlugin
    添加ModularGameplayActors为后续开发准备，来自Lyra

--- Source/OFYGame/OFYGame.Build.cs
+++ Source/OFYGame/OFYGame.Build.cs
@@ -7,10 +7,27 @@ public class OFYGame : ModuleRules
 	public OFYGame(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[]
+			{
+				"OFYGame"
+			});
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"CoreUObject", 
+				"Engine", 
+				"InputCore"
+			});
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				
+			});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
