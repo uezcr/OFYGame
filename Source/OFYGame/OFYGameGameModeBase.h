c1bf1912c9c7b0bdd19286a3889bf2dcbb888124
uezcr <75730965+uezcr@users.noreply.github.com>
2023-02-01 20:09:42 +0800

    初始
    建立初始项目

---
+++ Source/OFYGame/OFYGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OFYGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OFYGAME_API AOFYGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
