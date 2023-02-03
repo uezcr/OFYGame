f96cff8c36fe478dc2bad0751c94bbad9145bbb3
uezcr <75730965+uezcr@users.noreply.github.com>
2023-02-03 15:10:49 +0800

    CommonPlugins
    添加CommonGame、UIExtension 、CommonUser插件

---
+++ Source/OFYGame/GameFeatures/GFAction_AddWidgets.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFeatures/GFAction_WorldActionBase.h"
+
+
+#include "GFAction_AddWidgets.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OFYGAME_API UGFAction_AddWidgets : public UGFAction_WorldActionBase
+{
+	GENERATED_BODY()
+	
+};
